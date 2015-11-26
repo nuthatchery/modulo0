0fc7e5d086ca124a25ed20b02f42a6afc116e09a
Anya Helene Bagge <anya@ii.uib.no>
2015-11-26 15:25:18 +0100

    Fix strange dependency problems

---
+++ modulo0/src/modulo0/Activator.java
@@ -0,0 +1,43 @@
+package modulo0;
+
+import org.nuthatchery.pica.resources.LanguageRegistry;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator implements BundleActivator {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "modulo0"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		LanguageRegistry.registerLanguage(Modulo0Language.getInstance());
+	}
+
+	public void start(BundleContext context) throws Exception {
+		plugin = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+	}
+
+}
